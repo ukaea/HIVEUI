9575c1b9b2c92abfee188cc474bd5e340515c644
Adam Parker <adam.parker@ukaea.uk>
2025-02-07 16:31:35 +0000

    Change margin-right style for buttons to use Tailwind classes directly

--- src/components/user.svelte
+++ src/components/user.svelte
@@ -39,7 +39,7 @@
 <div>
 	{#if data.status}
 		<Toggle let:on={open} let:toggle let:toggleOff>
-			<Button variant="fill" on:click={toggle} style="margin-right: 20px">
+			<Button variant="fill" on:click={toggle} class="mr-4">
 				{data.user?.name ?? 'User'}
 				<Menu {open} on:close={toggleOff} matchWidth>
 					<MenuItem>
