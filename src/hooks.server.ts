12ee91f926236ed4d66b9df18bcdf0ddde2ee19d
BDevT <18251613+BDevT@users.noreply.github.com>
2025-11-06 01:46:10 +0000

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,10 +2,11 @@ import { redirect, type Handle } from '@sveltejs/kit';
 import { handle as authenticationHandle } from './auth';
 import { sequence } from '@sveltejs/kit/hooks';
 import { AUTH_REQUIRED_GROUP } from '$env/static/private';
+import { AUTH_ENABLE } from '$env/static/private';
 
 async function authorizationHandle({ event, resolve }) {
   // Protect any routes under /
-  if (event.url.pathname.startsWith('/')) {
+  if (event.url.pathname.startsWith('/') && AUTH_ENABLE === 'true') {
     const session = await event.locals.auth();
     if (!session) {
       // Redirect to the signin page
