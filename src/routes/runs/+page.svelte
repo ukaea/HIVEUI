346d02633621ef8dde43261482c6b208edb83ed1
BDevT <18251613+BDevT@users.noreply.github.com>
2026-03-27 11:47:22 +0000

    Add configuration local option to runs

--- src/routes/runs/+page.svelte
+++ src/routes/runs/+page.svelte
@@ -37,7 +37,7 @@
 
 	const configurationService = new GenericDataService<ConfigurationMetadata>({
 		modelClass: ConfigurationMetadataModel,
-		endpoint: '/db/configurations',
+		endpoint: env.PUBLIC_CONFIGURATION_LOCAL_STORAGE === 'true' ? '/local/configurations' : '/db/configurations',
 		idField: 'configurationId',
 		displayName: 'configurations'
 	});
